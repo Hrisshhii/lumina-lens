d3f8604c44a43d0849a4e390e3e64f4296c4be92
Hrishi <rathodhrishi12345@gmail.com>
2026-08-01 23:11:58 +0530

    Simplify View component formatting in App.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -8,13 +8,7 @@ export default function App() {
   }, []);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={{flex: 1,justifyContent: "center",alignItems: "center",}}>
       <Text>Lumina Lens 🚀</Text>
     </View>
   );
