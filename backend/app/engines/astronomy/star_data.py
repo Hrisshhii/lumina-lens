a17a66d7a6e0c287bad5f1812bf1ace0b9e8d9b5
Hrishi <rathodhrishi12345@gmail.com>
2026-08-14 19:35:11 +0530

    Add star data for key astronomical objects

---
+++ backend/app/engines/astronomy/star_data.py
@@ -0,0 +1,26 @@
+STARS=[
+  {
+    "name": "Sirius",
+    "ra_hours": 6 + 45 / 60 + 8.9 / 3600,
+    "dec_degrees": -(16 + 42 / 60 + 58 / 3600),
+    "magnitude": -1.46,
+  },
+  {
+    "name": "Vega",
+    "ra_hours": 18 + 36 / 60 + 56.3 / 3600,
+    "dec_degrees": 38 + 47 / 60 + 1 / 3600,
+    "magnitude": 0.03,
+  },
+  {
+    "name": "Betelgeuse",
+    "ra_hours": 5 + 55 / 60 + 10.3 / 3600,
+    "dec_degrees": 7 + 24 / 60 + 25 / 3600,
+    "magnitude": 0.42,
+  },
+  {
+    "name": "Polaris",
+    "ra_hours": 2 + 31 / 60 + 49 / 3600,
+    "dec_degrees": 89 + 15 / 60 + 51 / 3600,
+    "magnitude": 1.98,
+  },
+]
